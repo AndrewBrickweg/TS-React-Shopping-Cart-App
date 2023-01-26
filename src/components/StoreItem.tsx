257f90befc3dc5d6e6f531c11a3583430fffc0ef
Andrew Brickweg <Andrewcbrickweg@icloud.com>
2023-01-25 20:25:31 -0500

    added add to cart

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,15 +1,22 @@
 import { Button, Card } from "react-bootstrap";
+import { useShoppingCart } from "../context/ShoppingCartContext";
 import { formatCurrency } from "../util/formatCurrency";
 
-interface StoreItemProps {
+type StoreItemProps = {
   id: number;
   name: string;
   price: number;
   imgUrl: string;
-}
+};
 
 export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
-  const quantity = 0;
+  const {
+    getItemQuantity,
+    increaseCartQuantity,
+    decreaseCartQuantity,
+    removeFromCart,
+  } = useShoppingCart();
+  const quantity = getItemQuantity(id);
 
   return (
     <Card className="h-100">
@@ -26,19 +33,31 @@ export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
         </Card.Title>
         <div className="mt-auto">
           {quantity === 0 ? (
-            <Button className="w-100">+ Add To Cart</Button>
+            <Button className="w-100" onClick={() => increaseCartQuantity(id)}>
+              + Add To Cart
+            </Button>
           ) : (
             <div
               className="d-flex align-items-center flex-column"
-              style={{ gap: "0.5rem" }}
+              style={{ gap: ".5rem" }}
             >
-              bye
               <div
                 className="d-flex align-items-center justify-content-center"
-                style={{ gap: "0.5rem" }}
+                style={{ gap: ".5rem" }}
               >
-                Hi
+                <Button onClick={() => decreaseCartQuantity(id)}>-</Button>
+                <div>
+                  <span className="fs-3">{quantity}</span> in cart
+                </div>
+                <Button onClick={() => increaseCartQuantity(id)}>+</Button>
               </div>
+              <Button
+                onClick={() => removeFromCart(id)}
+                variant="danger"
+                size="sm"
+              >
+                Remove
+              </Button>
             </div>
           )}
         </div>
